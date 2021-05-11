4dd8e8b9682dd6bc3ccf44956809498139ceb77b
Justin Merrell <justin.merrell@adiaon.com>
2021-05-10 22:22:24 -0400

    wip: install npm

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ set -m              #Set Job Control
 #                                    Web UI                                    #
 # ---------------------------------------------------------------------------- #
 sudo apt-get dist-upgrade -y nodejs
+sudo apt-get dist-upgrade -y npm
 sudo npm install npm –global
 sudo npm install harrogate
